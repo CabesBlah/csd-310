3be27e520bd17ccf541a538ac6f4af0469efb15a
Caleb <caleb.lewandowski@gmail.com>
2021-02-05 18:50:50 -0600

    Module 6.2 Assignment + Module 5. Module 5 programs corrected errors presented by teacher.

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -9,16 +9,70 @@ client = MongoClient(url)
 db = client.pytech
 students = db.students
 
+student_bob = {
+	"student_id":"1007",
+	"first_name":"Bob",
+	"last_name":"Bank",
+	"enrollments": [{
+		"term":"Summer",
+		"gpa":"4.0",
+		"start_date":"6/1/2019",
+		"end_date":"8/28/2019",
+		"courses":[{
+			"course_id":"23456",
+			"description":"Java 101",
+			"instructor":"Someone Important",
+			"grade":"A"
+	    }]
+    }]
+}
+
+student_dude = {
+	"student_id":"1008",
+	"first_name":"Dude",
+	"last_name":"Done",
+	"enrollments": [{
+		"term":"Summer",
+		"gpa":"3.5",
+		"start_date":"6/1/2019",
+		"end_date":"8/28/2019",
+		"courses":[{
+			"course_id":"23456",
+			"description":"Java 101",
+			"instructor":"Someone Important",
+			"grade":"A-"
+		}]
+    }]
+}
+
+student_where = {
+	"student_id":"1009",
+	"first_name":"Where",
+	"last_name":"What",
+	"enrollments": [{
+		"term":"Summer",
+		"gpa":"3.0",
+		"start_date":"6/1/2019",
+		"end_date":"8/28/2019",
+		"courses":[{
+			"course_id":"23456",
+			"description":"Java 101",
+			"instructor":"Someone Important",
+			"grade":"B"
+		}]
+    }]
+} 
+
 #Insert student data.
 print("-- INSERT STATEMENTS --")
-new_student_object = {"student_id":1007, "first_name":"Bob", "last_name":"Bank"}
-new_student_id = students.insert_one(new_student_object).inserted_id
-print("Inserted student record", new_student_object["first_name"], new_student_object["last_name"], "into the students collection with document_id", new_student_id)
-new_student_object = {"student_id":1008, "first_name":"Dude", "last_name":"Done"}
-new_student_id = students.insert_one(new_student_object).inserted_id
-print("Inserted student record", new_student_object["first_name"], new_student_object["last_name"], "into the students collection with document_id", new_student_id)
-new_student_object = {"student_id":1009, "first_name":"Where", "last_name":"What"}
-new_student_id = students.insert_one(new_student_object).inserted_id
-print("Inserted student record", new_student_object["first_name"], new_student_object["last_name"], "into the students collection with document_id", new_student_id)
+
+new_student_id = students.insert_one(student_bob).inserted_id
+print("Inserted student record", student_bob["first_name"], student_bob["last_name"], "into the students collection with document_id", new_student_id)
+
+new_student_id = students.insert_one(student_dude).inserted_id
+print("Inserted student record", student_dude["first_name"], student_dude["last_name"], "into the students collection with document_id", new_student_id)
+
+new_student_id = students.insert_one(student_where).inserted_id
+print("Inserted student record", student_where["first_name"], student_where["last_name"], "into the students collection with document_id", new_student_id)
 
 input("\n\nEnd of program, press any key to exit...")
--- module_6/pytech_update.py
+++ module_6/pytech_update.py
@@ -1,6 +1,6 @@
 #Caleb Lewandowski
-#February 1, 2021
-#Module 5.3 Assignment
+#February 5, 2021
+#Module 6.2 Assignment
 
 #Set up connection.
 from pymongo import MongoClient
@@ -9,7 +9,7 @@ client = MongoClient(url)
 db = client.pytech
 students = db.students
 
-#Display all student data.
+#Display data of all students.
 print("-- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
 docs = db.students.find({})
 for doc in docs:
@@ -18,9 +18,12 @@ for doc in docs:
     print("Last Name:", doc["last_name"])
     print()
 
-#Display one set of student data.
-print("\n-- DISPLAYING STUDENT DOCUMENT FROM find_one() QUERY --")
-doc = db.students.find_one({"student_id":"1008"})
+#Update data of Student 1007.
+result = db.students.update_one({"student_id": "1007"}, {"$set": {"last_name":"Beep"}})
+
+#Display data of Student 1007.
+print("\n-- DISPLAYING STUDENT DOCUMENT 1007 --")
+doc = db.students.find_one({"student_id":"1007"})
 print("Student ID:", doc["student_id"])
 print("First Name:", doc["first_name"])
 print("Last Name:", doc["last_name"])
